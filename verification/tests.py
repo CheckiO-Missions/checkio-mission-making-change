1a88520c9057bcaadd05a88309d73931f4e809e6
ddifranc <david_difranco@yahoo.com>
2016-03-21 12:36:48 -0500

    Update tests.py

--- verification/tests.py
+++ verification/tests.py
@@ -11,29 +11,29 @@ Each test is dict with
 TESTS = {
     "Basics": [
         {
-            "input": [8, [1, 3, 5]],
+            "input": (8, [1, 3, 5]),
             "answer": 2,
             "explanation": "Outer Leftopian shillings"
         },
         {
-            "input": [12, [1, 4, 5]],
+            "input": (12, [1, 4, 5]),
             "answer": 3,
             "explanation": "East Frombazian pesos"
         }
     ],
     "Extra": [
         {
-            "input": [1, [3, 4, 5]],
+            "input": (1, [3, 4, 5]),
             "answer": None,
             "explanation": "New Oldhemian dollars"
         },
         {
-            "input": [4, [3, 5]],
+            "input": (4, [3, 5]),
             "answer": None,
             "explanation": "Democratic Turnlandian grommits"
         },
         {
-            "input": [123456, [1, 6, 7, 456, 678]],
+            "input": (123456, [1, 6, 7, 456, 678]),
             "answer": 187,
             "explanation": "South Northcambrian pounds"
         }
