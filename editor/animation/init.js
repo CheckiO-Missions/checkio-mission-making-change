cd79a87dc280570e94b3dc51221f2ca0106d04b6
ddifranc <david_difranco@yahoo.com>
2016-03-21 14:05:10 -0500

    Update init.js

--- editor/animation/init.js
+++ editor/animation/init.js
@@ -43,7 +43,8 @@ requirejs(['ext_editor_1', 'jquery_190', 'raphael_210', 'snap.svg_030'],
             var fname = 'checkio';
 
             var checkioInput = data.in;
-            var checkioInputStr = fname + '(' + JSON.stringify(checkioInput) + ')';
+            //var checkioInputStr = fname + '(' + JSON.stringify(checkioInput) + ')';
+            var checkioInputStr = fname + '(' + JSON.stringify(checkioInput[0]) + ", "+ checkioInput[1] + ')';
 
             var failError = function (dError) {
                 $content.find('.call').html(checkioInputStr);
