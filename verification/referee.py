26d75331172c505b3fe1cc2cf24aeb02ee190e1a
Alexander Lyabah <a.lyabah@checkio.org>
2019-05-03 23:09:04 +0300

    js

--- verification/referee.py
+++ verification/referee.py
@@ -39,10 +39,13 @@ api.add_listener(
     CheckiOReferee(
         tests=TESTS,
         cover_code={
-            'python-27': cover_codes.unwrap_args,  # or None
-            'python-3': cover_codes.unwrap_args
+            'python-3': cover_codes.unwrap_args,
+            'js-node': cover_codes.js_unwrap_args
         },
-        function_name="checkio"
+        function_name={
+            "python": "checkio",
+            "js": "makingChange"
+        },
         # checker=None,  # checkers.float.comparison(2)
         # add_allowed_modules=[],
         # add_close_builtins=[],
